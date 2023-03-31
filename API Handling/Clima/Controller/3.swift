baf95803057b237bb9e9e6b7941d130eecc5cb34
M.Taimoor Hassan <mtaimoor.tbox@gmail.com>
2023-03-31 16:49:17 +0500

    Dark and light Mode in Application , TextField Handling in details
    Dark and light Mode in Application , TextField Handling in details

--- API Handling/Clima/Controller/3.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ViewController.swift
-//  Clima
-//
-//  Created by M Taimoor Hassan on 01/09/2019.
-//  Copyright © 2019 App Brewery. All rights reserved.
-//
-
-
--- Weather App API/Weather App API/ViewController.swift
+++ Weather App API/Weather App API/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var location: UILabel!
+    @IBOutlet weak var temperature: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var searchTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
+        temperature.text = "22"
+        location.text = "Pakistan"
     }
 
-
+    @IBAction func searchBtnPressed(_ sender: Any) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text ?? "Pakistan" )
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text ?? "Pakistan" )
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+    }
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if searchTextField.text != "" {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    @IBAction func myLocationBtnPressed(_ sender: Any) {
+    }
 }
 
